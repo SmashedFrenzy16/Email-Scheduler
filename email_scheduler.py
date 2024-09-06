eee9a9fa834b3e57ed4e81825b83b6685988c376
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2024-09-06 10:11:30 +0100

    Update email_scheduler.py with inputs

--- email_scheduler.py
+++ email_scheduler.py
@@ -2,6 +2,19 @@ import smtplib
 import schedule
 import time
 
+r_email = input("Enter in the recipient's email address: ")
+    
+# Email credentials
+email = input("Enter in your email address: ")
+email_pass = input("Enter in your email password: ")
+
+# Email server
+smtp = input("Enter in your email SMTP server: ")
+port = input("Enter in your email SMTP port: ")
+
+# Email message
+
+message = input("Enter in your email message: ")
 
 def send_email():
 
